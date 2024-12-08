e42434484f55e3325d537d0a95553e5b7360862a
Eric Mullen <MullenProgramming@gmail.com>
2024-12-08 22:16:30 +0100

    Create solution.java

---
+++ HackerRankChallenges/Java_Loops_II/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt(); //Num queries
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for(int j = 1; j <= n; j++){
+                int result = a + ((int) Math.pow(2, j) * b);
+                System.out.printf("%d ", result - b);
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
