4dc2009cc84c2c0d2b5c1ec9d04a997dab212e31
Eric Mullen <MullenProgramming@gmail.com>
2024-12-12 18:20:31 +0100

    Create solution.java

---
+++ HackerRankChallenges/Primality/solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String n = bufferedReader.readLine();
+        
+// ******************* MY INPUT STARTS HERE *****************
+        BigInteger big = new BigInteger(n);
+        if(big.isProbablePrime(1)){
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+// **********************************************************                 
+        bufferedReader.close();
+    }
+}
