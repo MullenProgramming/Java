9e7e4b2242a80c1b7462ff28f17ba1f2172d76c6
Eric Mullen <MullenProgramming@gmail.com>
2025-01-07 12:17:23 +0100

    Update solution.java

--- HackerRankChallenges/Regex/solution.java
+++ HackerRankChallenges/Regex/solution.java
@@ -17,3 +17,5 @@ class Solution{
 class MyRegex {
     String pattern = "^(25[0-5]|2[0-4]\\d|[01]?\\d?\\d)\\.(25[0-5]|2[0-4]\\d|[01]?\\d?\\d)\\.(25[0-5]|2[0-4]\\d|[01]?\\d?\\d)\\.(25[0-5]|2[0-4]\\d|[01]?\\d?\\d)$";
 }
+
+// I used a regex generator for the regex pattern. I admit I would not be able to solve this on my own without that tool.
