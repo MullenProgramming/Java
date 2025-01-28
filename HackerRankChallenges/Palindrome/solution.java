26b85bde8e9143d2cac92f54717bb1a0b7358cf3
Eric Mullen <MullenProgramming@gmail.com>
2025-01-28 20:37:40 +0100

    Delete HackerRankChallenges/Palindrome directory

--- HackerRankChallenges/Palindrome/solution.java
+++
@@ -1,8 +0,0 @@
-// This is a very inefficient solution as far as speed is concerned.
-
-class Solution {
-    public boolean isPalindrome(String s) {
-        String str = s.toLowerCase().trim().replaceAll("[^a-z0-9]", "");
-        return str.equals(new StringBuilder().append(str).reverse().toString());
-    }
-}
